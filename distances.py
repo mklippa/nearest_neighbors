f8bb936f8ff42deaf4891f7a7555ba95f96d04e7
Maxim Klippa <mklippa@ozon.ru>
2019-11-01 14:06:50 +0300

    add check

--- distances.py
+++ distances.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.spatial.distance import pdist 
+from scipy.spatial.distance import pdist
 
 
 def euclidean_distance(x, y):
@@ -11,19 +11,16 @@ def euclidean_distance(x, y):
             for k in range(len(x[i])):
                 s += (x[i][k] - y[j][k]) ** 2
             dist[i].append(s ** (1 / 2))
+            print(f"debug: {pdist(np.array([x[i], y[j]]))}")
+            print(f"my: {dist[i][-1]}")
     return dist
 
 
-# x = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20]]
-# y = [[8, 7, 6, 5], [4, 3, 2, 1]]
+x = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20]]
+y = [[8, 7, 6, 5], [4, 3, 2, 1]]
 
-x = [[1, 2]]
-y = [[5, 6]]
-print(euclidean_distance(x,y))
-# print(pdist(x))
+print(euclidean_distance(x, y))
+
 
 def cosine_distance(x, y):
-    #####################
-    # your code is here #
-    #####################
     pass
