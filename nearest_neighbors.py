450f7e89398d85bde6e81893971de6437bccc25c
Maxim Klippa <mklippa@ozon.ru>
2019-11-02 11:40:56 +0300

    own method

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -1,4 +1,5 @@
 import numpy as np
+import distances as dst
 
 EPS = 1e-5
 
@@ -12,17 +13,26 @@ class KNNClassifier:
         self.test_block_size = test_block_size
     
     def fit(self, X, y):
-        #####################
-        # your code is here #
-        #####################
+        self.train_X = X
+        self.train_y = y
 
     def find_kneighbors(self, X, return_distance):
-        #####################
-        # your code is here #
-        #####################
-    
+        all_distances = dst.euclidean_distance(X, self.train_X)
+        k_distances = []
+        k_indexes = []
+        for i in range(len(X)):
+            el_distances = [(index, value) for index, value in enumerate(all_distances[i])]
+            el_distances.sort(key=lambda x: x[1])
+            k_distances.append(map(lambda x: x[1], el_distances[:self.k]))
+            k_indexes.append(map(lambda x: x[0], el_distances[:self.k]))
+        if return_distance:
+            return (np.array(k_distances), np.array(k_indexes))
+        else:
+            return np.array(k_indexes)
+
     def predict(self, X):
         #####################
         # your code is here #
         #####################
+        pass
 
