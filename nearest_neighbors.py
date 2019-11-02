4096f8cc55ad0bef9ab0289e501c110dd0a73b9c
Maxim Klippa <mklippa@ozon.ru>
2019-11-02 16:53:22 +0300

    wighted predicat

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -12,7 +12,7 @@ class KNNClassifier:
         self.metric = metric
         self.weights = weights
         self.test_block_size = test_block_size
-    
+
     def fit(self, X, y):
         self.train_X = X
         self.train_y = y
@@ -22,20 +22,49 @@ class KNNClassifier:
         k_distances = []
         k_indices = []
         for i in range(len(X)):
-            el_distances = [(index, value) for index, value in enumerate(all_distances[i])]
+            el_distances = [
+                (index, value) for index, value in enumerate(all_distances[i])
+            ]
             el_distances.sort(key=lambda x: x[1])
-            k_distances.append(map(lambda x: x[1], el_distances[:self.k]))
-            k_indices.append(map(lambda x: x[0], el_distances[:self.k]))
+            k_distances.append(map(lambda x: x[1], el_distances[: self.k]))
+            k_indices.append(map(lambda x: x[0], el_distances[: self.k]))
         if return_distance:
             return (np.array(k_distances), np.array(k_indices))
         else:
             return np.array(k_indices)
 
     def predict(self, X):
-        kneighbors = self.find_kneighbors(X)
+        distances, indices = self.find_kneighbors(X)
+
         y = []
-        for i in range(len(kneighbors)):
-            classes = [self.train_y[j] for j in kneighbors[i]]
-            y.append(mode(classes))
+        for i in range(len(indices)):
+            classes, weights = [
+                (self.train_y[k], 1 / (distances[i][j] + EPS))
+                for j, k in enumerate(indices[i])
+            ]
+
+            class_counter = {}
+            for c_w in zip(classes, weights):
+                if class_counter[c_w]:
+                    class_counter[c_w][0] += 1
+                    class_counter[c_w][1] += c_w[1]
+                else:
+                    class_counter[c_w] = (1, c_w[1])
+
+            frequent_class = max(class_counter.items(), key=lambda x: x[1][0])[0]
+            heavy_class = max(class_counter.items(), key=lambda x: x[1][1])[0]
+
+            if self.weights:
+                y.append(heavy_class)
+            else:
+                y.append(frequent_class)
         return y
+
+        # kneighbors = self.find_kneighbors(X)
+        # y = []
+        # for i in range(len(kneighbors)):
+        #     weights = kneighbors
+        #     classes = [self.train_y[j] for j in kneighbors[i]]
+        #     y.append(mode(classes))
+        # return y
 
