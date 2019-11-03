6e276277acddf154ae6e0fd0891dc3a7e0788ca5
Maxim Klippa <mklippa@ozon.ru>
2019-11-03 10:49:18 +0300

    cross validation

--- cross_validation.py
+++ cross_validation.py
@@ -1,4 +1,18 @@
+from nearest_neighbors import KNNClassifier
+from sklearn.model_selection import KFold
+
 def knn_cross_val_score(X, y, k_list, score, cv=None, **kwargs):
-    #####################
-    # your code is here #
-    #####################
+    knn = KNNClassifier(**kwargs)
+    res = {}
+    for k in k_list:
+        res[k] = []
+        kf = KFold(k)
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            knn.fit(X_train, y_train)
+            my_y = knn.predict(X_test)
+            correct = sum([1 for i,v in enumerate(my_y) if y_test[i] is v])
+            res[k].append(correct/len(my_y))
+
+    return res
