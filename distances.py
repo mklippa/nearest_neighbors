4fc22005d6b8edb003b9ed8c7d01798af064f0b5
Maxim Klippa <mklippa@ozon.ru>
2019-11-03 14:15:46 +0300

    remove trash

--- distances.py
+++ distances.py
@@ -15,13 +15,6 @@ def euclidean_distance(x, y):
             # print(f"my: {dist[i][-1]}")
     return dist
 
-
-x = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16], [17, 18, 19, 20]]
-# y = [[8, 7, 6, 5], [4, 3, 2, 1]]
-
-# print(euclidean_distance(x, y))
-
-
 def cosine_distance(x, y):
     dist = []
     for i in range(len(x)):
@@ -39,21 +32,3 @@ def cosine_distance(x, y):
             # print(f"debug: {pdist(np.array([x[i], y[j]]), 'cosine')}")
             # print(f"my: {dist[i][-1]}")
     return dist
-
-# print(cosine_distance(x, y))
-
-
-def foo(x):
-    dst = euclidean_distance(x,x)
-    print(dst)
-    a = {}
-    for i in range(len(x)):
-        for j in range(len(x)):
-            if i < j:
-                a[(i,j)] = dst[i][j]
-    
-    sorted_a = sorted(a.items(), key=lambda kv: kv[1])
-    for item in sorted_a:
-        print(item)
-
-foo(x)
