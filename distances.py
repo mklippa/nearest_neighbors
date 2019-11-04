6cb0765a6f7a8b37076769157b3c1fd09e3ec116
Maxim Klippa <mklippa@ozon.ru>
2019-11-04 19:46:20 +0300

    distances with one loop

--- distances.py
+++ distances.py
@@ -2,12 +2,20 @@ import numpy as np
 
 
 def euclidean_distance(x, y):
-    return np.sum((x[:, None] - y) ** 2, axis=2) ** 0.5
+    N = x.shape[0]
+    M = y.shape[0]
+    dists = np.zeros((N, M))
+    for i in range(M):
+        dists[:, i] = np.sum((x - y[i, :]) ** 2, axis=1) ** 0.5
+    return dists
 
 
 def cosine_distance(x, y):
-    a = x[:, None]
-    b = y
-    numerator = np.sum(a * b, axis=2)
-    denominator = np.sum(a ** 2, axis=2) ** 0.5 * np.sum(b ** 2, axis=1) ** 0.5
-    return 1 - numerator / denominator
+    N = x.shape[0]
+    M = y.shape[0]
+    dists = np.zeros((N, M))
+    for i in range(M):
+        numerator = np.sum(x * y[i, :], axis=1)
+        denominator = np.sum(x ** 2, axis=1) ** 0.5 * np.sum(y[i, :] ** 2) ** 0.5
+        dists[:, i] = 1 - numerator / denominator
+    return dists
