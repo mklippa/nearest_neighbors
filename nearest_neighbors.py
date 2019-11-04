13e439dfae9f186234b2e712ddcab0d60580b8de
Maxim Klippa <mklippa@ozon.ru>
2019-11-04 17:05:50 +0300

    find_kneighbors with numpy

--- nearest_neighbors.py
+++ nearest_neighbors.py
@@ -19,60 +19,59 @@ class KNNClassifier:
         if self.strategy is not "my_own":
             self.neigh.fit(X, y)
         else:
-            self.train_X = X
-            self.train_y = y
+            self.X = X
+            self.y = y
 
     def find_kneighbors(self, X, return_distance=False):
-        all_distances = dst.euclidean_distance(X, self.train_X)
-        k_distances = []
-        k_indices = []
-        for i in range(len(X)):
-            el_distances = [
-                (index, value) for index, value in enumerate(all_distances[i])
-            ]
-            el_distances.sort(key=lambda x: x[1])
-            k_distances.append(map(lambda x: x[1], el_distances[: self.k]))
-            k_indices.append(map(lambda x: x[0], el_distances[: self.k]))
-        if return_distance:
-            return (np.array(k_distances), np.array(k_indices))
+        if self.metric is "euclidean":
+            distances = dst.euclidean_distance(X, self.X)
         else:
-            return np.array(k_indices)
+            distances = dst.cosine_distance(X, self.X)
+
+        k_indices = np.argpartition(distances, -self.k, axis=1)[:, -self.k :]
+        k_distances = np.take_along_axis(distances, k_indices, axis=1)
+
+        if return_distance:
+            return (k_distances, k_indices)
+        else:
+            return k_indices
 
     def predict(self, X):
-        if self.strategy is not "my_own":
-            if self.weights:
-                distances, indices = self.neigh.kneighbors(X, return_distance=True)
-            else:
-                indices = self.neigh.kneighbors(X, return_distance=False)
-                distances = [1]*len(indices)
-        else:
-            if self.weights:
-                distances, indices = self.find_kneighbors(X, return_distance=True)
-            else:
-                indices = self.find_kneighbors(X, return_distance=False)
-                distances = [1]*len(indices)
+        pass
+        # if self.strategy is not "my_own":
+        #     if self.weights:
+        #         distances, indices = self.neigh.kneighbors(X, return_distance=True)
+        #     else:
+        #         indices = self.neigh.kneighbors(X, return_distance=False)
+        #         distances = [1]*len(indices)
+        # else:
+        #     if self.weights:
+        #         distances, indices = self.find_kneighbors(X, return_distance=True)
+        #     else:
+        #         indices = self.find_kneighbors(X, return_distance=False)
+        #         distances = [1]*len(indices)
 
-        y = []
-        for i in range(len(indices)):
-            classes, weights = [
-                (self.train_y[k], 1 / (distances[i][j] + EPS))
-                for j, k in enumerate(indices[i])
-            ]
+        # y = []
+        # for i in range(len(indices)):
+        #     classes, weights = [
+        #         (self.train_y[k], 1 / (distances[i][j] + EPS))
+        #         for j, k in enumerate(indices[i])
+        #     ]
 
-            class_counter = {}
-            for c_w in zip(classes, weights):
-                if class_counter[c_w]:
-                    class_counter[c_w][0] += 1
-                    class_counter[c_w][1] += c_w[1]
-                else:
-                    class_counter[c_w] = (1, c_w[1])
+        #     class_counter = {}
+        #     for c_w in zip(classes, weights):
+        #         if class_counter[c_w]:
+        #             class_counter[c_w][0] += 1
+        #             class_counter[c_w][1] += c_w[1]
+        #         else:
+        #             class_counter[c_w] = (1, c_w[1])
 
-            frequent_class = max(class_counter.items(), key=lambda x: x[1][0])[0]
-            heavy_class = max(class_counter.items(), key=lambda x: x[1][1])[0]
+        #     frequent_class = max(class_counter.items(), key=lambda x: x[1][0])[0]
+        #     heavy_class = max(class_counter.items(), key=lambda x: x[1][1])[0]
 
-            if self.weights:
-                y.append(heavy_class)
-            else:
-                y.append(frequent_class)
-        return y
+        #     if self.weights:
+        #         y.append(heavy_class)
+        #     else:
+        #         y.append(frequent_class)
+        # return y
 
